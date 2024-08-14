5291cd32e6bf60d8948e06247b32ac69ea146b6c
Abdulbasit <basttyy@gmail.com>
2024-08-14 13:21:01 +0100

    implement app entrypoint

---
+++ public/index.php
@@ -0,0 +1,67 @@
+<?php
+
+use Eyika\Atom\Framework\Http\Server;
+
+define('ATOM_START', microtime(true));
+
+$log_path = __DIR__."/../storage/logs/php_error.log";
+ini_set('error_log', $log_path);
+
+date_default_timezone_set('UTC');
+
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Register The Helper Functions
+|--------------------------------------------------------------------------
+|
+| The helper functions file provide couple of useful short helper functions
+| for performing regular computations.
+|
+*/
+require __DIR__."/src/libs/helpers.php";
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
+
+try {
+    $server = new Server(['env']);
+    $server->handle($_ENV['APP_BASE_PATH'] ?? dirname(__DIR__));
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
